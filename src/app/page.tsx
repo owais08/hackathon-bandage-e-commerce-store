c5de2aa2f757ce008aaf2cd3b1130574474b6d8a
owais08 <muhammadowais0811@gmail.com>
2024-12-10 02:17:19 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Header />
       <Navbar />
       <Carousel />
-      <Cards/>
+      <Cards />
       <EditorsPick />
       <FeaturedProducts />
       <Footer />
