9a03ce6a04d98817e94170726de4fcbea55171c9
owais08 <muhammadowais0811@gmail.com>
2025-01-11 04:13:37 +0500

    change files name (editors & featured )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "./components/header";
 import Hero from "./components/hero";
-import EditorsPick from "./components/editorsPick";
-import FeaturedProducts from "./components/featuredProducts";
 import PromoProduct from "./components/prormoProduct";
 import Article from "./components/article";
 import Posts from "./components/post";
 import Footer from "./components/footer";
+import EditorsPick from "./components/editorsPick";
+import FeaturedProducts from "./components/featuredProducts";
 
 export default function Home() {
   return (
